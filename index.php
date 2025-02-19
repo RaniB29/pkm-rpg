11687a9a5ac8019ab8df7acaac1b65ff3c03c7d8
Alan Raniel <raniel29c@gmail.com>
2025-02-19 13:54:03 -0300

    associativa arrays

--- index.php
+++ index.php
@@ -19,6 +19,38 @@ $playerPokemonList = [
     "Jigglypuff"
 ];
 
+$nameIndex = 0;
+$typeIndex = 1;
+$numberIndex = 2;
+$evolutionIndex = 3;
+
+$bulbassaurInfo = [
+    $nameIndex => "Bulbassaur",
+    $typeIndex => "grass/poison",
+    $numberIndex => 1,
+    $evolutionIndex => 1
+];
+
+$charmanderInfo = [
+    $nameIndex => "Charmander",
+    $typeIndex => "fire",
+    $numberIndex => 4,
+    $evolutionIndex => 1
+];
+
+$squirtleInfo = [
+    "name" => "Squirtle",
+    "type" => "water",
+    "number" => 7,
+    "evolution" => 1
+];
+
+$newPokemonList = [
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo
+];
+
 function colorir($message)
 {
     echo "\e[33m{$message}\e[0m";
@@ -55,3 +87,8 @@ if ($pokeballNumber > POKEBALL_LIMIT)
 
 speakMessage("Joy", "Great {$playerName}! I will heal your {$pokeballNumber} Pokemons.");
 speakMessage("Joy", "So, your $playerPokemonList[0], {$playerPokemonList[1]}, {$playerPokemonList[2]}, {$playerPokemonList[3]}, {$playerPokemonList[4]} and {$playerPokemonList[5]} has been healed");
+
+speakMessage("Joy", "You have a nice {$newPokemonList[0][$nameIndex]}, it number is {$newPokemonList[0][$numberIndex]}");
+speakMessage("Joy", "You have a nice {$newPokemonList[1][$nameIndex]}, it number is {$newPokemonList[1][$numberIndex]}");
+speakMessage("Joy", "You have a nice {$newPokemonList[2]['name']}, it number is {$newPokemonList[2]['number']}");
+//speakMessage("Joy", "You have a nice {$squirtleInfo['name']}, it number is {$squirtleInfo['number']}");
