94a890ce4dd8d9bc2a232a836f946ae6e78be273
Alan Raniel <raniel29c@gmail.com>
2025-02-20 14:16:40 -0300

    Operadores aritméticos

--- alan.php
+++ alan.php
@@ -1,12 +1,14 @@
 <?php
 
 $dividendo = $argv[1];
+// O sinal de igualdade = significa atribuição
+// Exemplo:     $minhaIdade = 36         nesse caso estou atribuindo o valor 36 para $minhaIdade
 $divisor = $argv[2];
 $quociente;
 
 if ($divisor != 0)
 {
-    $quociente = ($dividendo ^ $divisor);
+    $quociente = ($dividendo / $divisor);
     echo "Você digitou {$dividendo} para ser dividido por {$divisor}\n";
     echo "Logo, a resposta é igual à {$quociente}\n";
 }
@@ -16,3 +18,8 @@ else
     exit;
 }
 
+//Operadores aritméticos            
+    // multiplicação *
+    // divisão /
+    // adição +
+    // subtração -
