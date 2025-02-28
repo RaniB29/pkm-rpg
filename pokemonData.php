4db93a9e5c006328f7d548d083626a5ce3e855a1
Alan Raniel <raniel29c@gmail.com>
2025-02-28 13:46:41 -0300

    add pokemonData

---
+++ pokemonData.php
@@ -0,0 +1,82 @@
+<?php
+
+class PokeInfo
+{
+    public $name;
+    public $type;
+    public $number;
+    public $evolution;
+}
+
+$instance = new PokeInfo();
+$instance->name = "gilberto";
+$instance->type = "grass/posion";
+$instance->number = 2;
+$instance->evolution = 2;
+
+$instance2 = new PokeInfo();
+$instance2->name = "mary";
+$instance2->type = "grass/posion";
+$instance2->number = 2;
+$instance2->evolution = 2;
+
+//var_dump($instance->name);
+echo "The pokemon's name is {$instance->name}\n";
+echo "The pokemon's name is {$instance2->name}\n";
+
+//criando objetos da classe PokeInfo
+$bulbassaurInfo = new PokeInfo();
+$bulbassaurInfo->name = "Bulbassaur";
+$bulbassaurInfo->type = "grass/poison";
+$bulbassaurInfo->number = 1;
+$bulbassaurInfo->evolution = 1;
+
+$charmanderInfo = new PokeInfo();
+$charmanderInfo->name = "Charmander";
+$charmanderInfo->type = "fire";
+$charmanderInfo->number = 4;
+$charmanderInfo->evolution = 1;
+
+$squirtleInfo = new PokeInfo();
+$squirtleInfo->name = "Squirtle";
+$squirtleInfo->type = "water";
+$squirtleInfo->number = 7;
+$squirtleInfo->evolution = 1;
+
+/* criando um array associativo
+$charmanderInfo = [
+    "name" => "Charmander",
+    "type" => "fire",
+    "number" => 4,
+    "evolution" => 1
+];
+
+$squirtleInfo = [
+    "name" => "Squirtle",
+    "type" => "water",
+    "number" => 7,
+    "evolution" => 1
+];*/
+
+$pokemonList = [
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo,
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo,
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo,
+    $squirtleInfo,
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo,
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo,
+    $bulbassaurInfo,
+    $charmanderInfo,
+    $squirtleInfo,
+    $squirtleInfo,
+];
